19fb98cd450af4aff49406474f26aadd6c042b7a
Kartikey Srivastava <kartiksrivastava2307@gmail.com>
2025-07-18 21:56:26 +0530

    Update query1.sql

--- query1.sql
+++ query1.sql
@@ -1,1 +1,8 @@
-
+CREATE OR REPLACE TRIGGER trg_no_negative_salary
+BEFORE INSERT OR UPDATE OF salary ON employees
+FOR EACH ROW
+BEGIN
+    IF :NEW.salary < 0 THEN
+        RAISE_APPLICATION_ERROR(-20001, 'Salary cannot be negative');
+    END IF;
+END;
