8f5395659009f152dea6f5dbfaf31bf63d3b0838
Kartikey Srivastava <kartiksrivastava2307@gmail.com>
2025-07-20 04:26:35 +0530

    Update sum.sql

--- sum.sql
+++ sum.sql
@@ -1,9 +1,7 @@
 DECLARE
-  num1 number(10);
-  num2 number(10);
+  num1 number(10):=&num1;
+  num2 number(10):=&num2;
 BEGIN 
-  num1:=11;
-  num2:=2;
   dbms_output.put_line('sum = ' || (num1 + num2)); 
 END;
 /
