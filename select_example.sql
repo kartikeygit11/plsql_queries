65dff6aadff9fb27afe2154df02cad3c4acd817f
Kartikey Srivastava <kartiksrivastava2307@gmail.com>
2025-07-20 04:49:04 +0530

    Create select_example.sql

---
+++ select_example.sql
@@ -0,0 +1,27 @@
+
+CREATE TABLE employee (
+    serial_no NUMBER PRIMARY KEY,
+    name VARCHAR2(50),
+    age NUMBER,
+    gender VARCHAR2(10)
+);
+
+
+CREATE SEQUENCE emp_seq
+START WITH 1
+INCREMENT BY 1
+NOCACHE;
+
+INSERT INTO employee (serial_no, name, age, gender) VALUES (emp_seq.NEXTVAL, 'Alice', 30, 'Female');
+INSERT INTO employee (serial_no, name, age, gender) VALUES (emp_seq.NEXTVAL, 'Bob', 28, 'Male');
+INSERT INTO employee (serial_no, name, age, gender) VALUES (emp_seq.NEXTVAL, 'Charlie', 35, 'Male');
+INSERT INTO employee (serial_no, name, age, gender) VALUES (emp_seq.NEXTVAL, 'Diana', 26, 'Female');
+INSERT INTO employee (serial_no, name, age, gender) VALUES (emp_seq.NEXTVAL, 'Ethan', 40, 'Male');
+
+DECLARE
+  message varchar2(100); 
+BEGIN 
+  select age into message from employee where serial_no=3;
+  dbms_output.put_line(message); 
+END;
+/
